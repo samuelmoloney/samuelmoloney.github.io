5e22eeb53d6d7babde4fcf408034b5a96912bf3c
Samuel moloney <s.moloney@thisisaura.com>
2024-09-02 20:04:41 +1000

    update flocking

--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -1,12 +1,13 @@
 // background.ts
 
 import * as THREE from 'three';
+import { Flock } from './flocking';
 
 const canvas = document.getElementById("backgroundCanvas") as HTMLCanvasElement;
 let scene: THREE.Scene;
 let camera: THREE.PerspectiveCamera;
 let renderer: THREE.WebGLRenderer;
-let cube: THREE.Mesh;
+
 
 function onWindowResize(){
 
@@ -34,8 +35,8 @@ function init(): void {
     // Hooks
     window.addEventListener( 'resize', onWindowResize, false );
 
-    // Add a cube when the page loads
-    addCube();
+    // Create a new instance of Flock
+    new Flock(scene, camera, renderer);
     
     // Set up animation loop
     requestAnimationFrame(draw);
@@ -44,40 +45,16 @@ function init(): void {
 }
 
 function draw(): void {
-    
-    console.log("Draw Called");
+
     // Render the scene
     renderer.render(scene, camera);
-    
+
     // Request next frame
     requestAnimationFrame(draw);
-
-  
 }
 
-// Function to add objects to the scene
-function addCube(): void { 
-    
-    console.log("addCube");
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial({color: THREE.Color.NAMES.blue});
-    cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-
-   
-}
 
 // Initialize everything
 window.addEventListener("load", init);
 
-
-// Animate the cube
-function animate(): void {
-    requestAnimationFrame(animate);
-    
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
-    console.log("Animate");
-}
 
